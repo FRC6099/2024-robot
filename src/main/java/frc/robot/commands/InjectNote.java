b124269d5908904965dec4208aca43db0c32c9d4
knightriders6099 <knightriders6099@gmail.com>
2024-03-15 13:25:50 -0400

    Fix logic again

--- src/main/java/frc/robot/commands/InjectNote.java
+++ src/main/java/frc/robot/commands/InjectNote.java
@@ -37,6 +37,6 @@ public class InjectNote extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return this.intake.wasNoteNotPresent();
+    return !this.intake.wasNoteNotPresent();
   }
 }
